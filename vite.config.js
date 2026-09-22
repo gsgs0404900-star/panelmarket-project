05e309449f8e927b3c7e78bb7580a056ce6f869e
gsgs0404900-star <gsgs0404900@gmail.com>
2026-09-22 17:32:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,27 +9,5 @@ export default defineConfig({
         secure: false
       }
     }
-  },
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        login: 'login.html',
-        register: 'register.html',
-        detail: 'detail.html',
-        fiyatlar: 'fiyatlar.html',
-        hakkimizda: 'hakkimizda.html',
-        hesabim: 'hesabim.html',
-        kategoriler: 'kategoriler.html',
-        paneller: 'paneller.html',
-        support: 'support.html',
-        admin: 'admin.html',
-        payment: 'payment.html',
-        panelmarket: 'panelmarket.html'
-      }
-    }
   }
 });
-
-// PanelMarket Vercel multi-page build
