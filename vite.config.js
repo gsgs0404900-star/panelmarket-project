cdf75b01861096a75134a9e4fd1b51b55884bf8c
gsgs0404900-star <gsgs0404900@gmail.com>
2026-09-22 19:07:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,7 @@ export default {
         index: "index.html",
         login: "login.html",
         register: "register.html",
-        hesabim: "hesabim.html",
-        paneller: "paneller.html",
-        kategoriler: "kategoriler.html",
-        fiyatlar: "fiyatlar.html",
-        hakkimizda: "hakkimizda.html",
-        detail: "detail.html",
-        payment: "payment.html",
-        support: "support.html",
-        admin: "admin.html"
+        hesabim: "hesabim.html"
       }
     }
   }
